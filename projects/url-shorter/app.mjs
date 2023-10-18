26a81426b1f387879136f64e84cfd2b256819867
Vibhore <onlyvibhore@gmail.com>
2023-10-18 22:35:34 +0530

    url shortner added

---
+++ projects/url-shorter/app.mjs
@@ -0,0 +1,66 @@
+import express from "express";
+const app = express();
+
+import mongoose from "mongoose";
+
+import { nanoid } from 'nanoid';
+import cors from "cors";
+import bodyParser from "body-parser";
+
+
+app.set("view engine", "ejs");
+
+mongoose.connect("mongodb+srv://onlyvibhore:vanshu27@cluster0.tewksvm.mongodb.net/?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const UrlSchema = new mongoose.Schema({
+  longUrl: String,
+  shortKey: String
+});
+const Url = mongoose.model('Url', UrlSchema);
+
+app.use(express.json());
+app.use(cors()); 
+
+app.post('/shorten', async (req, res) => {
+  // Code for creating short URLs...
+});
+
+
+app.get("/", function(req,res){
+  res.render("index.ejs");
+});
+
+app.post("/",async(req,res)=>{
+
+ const {longUrl} =  req.body;
+ const shortKey = nanoid(7);
+ await Url.create({ longUrl: longUrl, shortKey });
+ const shortUrl = `http://localhost:400/${shortKey}`;
+ res.json({ shortUrl });
+
+});
+
+app.get('/:shortKey', async (req, res) => {
+  const {shortKey}   = req.params;
+  console.log('Short Key:', shortKey);
+  const url = await Url.findOne({ shortKey });
+
+  if (url) {
+    res.redirect(url.longUrl);
+  } else {
+    res.status(404).json({ error: 'URL not found' });
+  }
+});
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
+
+
+  app.listen("400", (req,res)=>{
+    console.log("Server is running on port 400");
+   
+    
+});
